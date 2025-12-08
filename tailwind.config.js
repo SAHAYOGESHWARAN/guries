7ff589c8966018dfec80f09e2e06566fda1fe68d
sahayogeshwaranp <155245416+SAHAYOGESHWARAN@users.noreply.github.com>
2025-12-08 10:49:28 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./views/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./hooks/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./index.tsx",
   ],
   theme: {
     extend: {
