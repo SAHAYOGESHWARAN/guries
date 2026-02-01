3dfd2107c62226b79f4f68e64fba1d4aacd5ed19
sahayogeshwaranp <155245416+SAHAYOGESHWARAN@users.noreply.github.com>
2026-02-01 10:58:29 +0530

    ne

--- api/v1/notifications.js
+++
@@ -1,9 +0,0 @@
-module.exports = function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-    if (req.method === 'OPTIONS') return res.status(200).json({ ok: true });
-    if (req.method === 'GET') return res.status(200).json([]);
-    return res.status(405).json({ error: 'Method not allowed' });
-};
